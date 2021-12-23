1d6bb65b9d87f0333eb36798c94d778596ad2436
Sameep Sehgal <sameepsam34@gmail.com>
2021-12-23 10:55:13 +0530

    Added containers Icons

---
+++ app/src/main/java/com/example/myapplication/utils/Container.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.utils
+
+import com.example.myapplication.R
+
+class Container {
+  companion object {
+    const val BOTTLE = R.drawable.soda_bottle_5
+    const val MUG = R.drawable.mug_3
+    const val GLASS = R.drawable.glass_2
+    const val CUP = R.drawable.cup_1
+    //TODO("Add more containers here")
+  }
+}
