407c074901116f1182fff481bf564f4fde3f9bc3
Sameep Sehgal <sameepsam34@gmail.com>
2022-01-06 07:09:31 +0530

    Added scroll to right in DataGraph but doesnt work now

--- app/src/main/java/com/example/myapplication/ui/screens/historytab/components/DataGraph.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/historytab/components/DataGraph.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication.ui.screens.historytab.components
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.PreferenceDataStoreViewModel
+import com.example.myapplication.R
 import com.example.myapplication.RoomDatabaseViewModel
 import com.example.myapplication.data.models.DailyWaterRecord
 import com.example.myapplication.utils.DateString
@@ -34,34 +38,49 @@ fun DataGraph(
   var currDate = DateString.getTodaysDate()
 
   Log.d("TAG", "DataGraph: ${firstWaterRecordDate.value}")
-  LazyRow(
-    reverseLayout = true,
-    state = listState,
-    modifier = Modifier.padding(0.dp, 8.dp)
+  
+  Box(
+    contentAlignment = Alignment.BottomEnd
   ) {
-    if(firstWaterRecordDate.value != DateString.NOT_SET) {
-      //Compose composable only when data is loaded
-      items(count = 29) {
-        while(currDate >= firstWaterRecordDate.value) {
-          val tempDate = currDate
-          roomDatabaseViewModel.getDailyWaterRecord(currDate)
-          Log.d("TAG", "DataGraph: $currDate - ${dateRecordMapper.value[currDate]}")
-          dateRecordMapper.value[currDate]?.date?.let { it1 ->
-            dateRecordMapper.value[currDate]?.currWaterAmount?.let { it2 ->
-              dateRecordMapper.value[currDate]?.goal?.let { it3 ->
-                Bar(
-                  date = it1,
-                  waterAmount = it2,
-                  waterGoal = it3,
-                  setSelectedDate = setSelectedDate,
-                  selectedDate = selectedDate
-                )
+    LazyRow(
+      reverseLayout = true,
+      state = listState,
+      modifier = Modifier.padding(0.dp, 8.dp)
+    ) {
+      if(firstWaterRecordDate.value != DateString.NOT_SET) {
+        //Compose composable only when data is loaded
+        items(count = 33) {
+          while(currDate >= firstWaterRecordDate.value) {
+            val tempDate = currDate
+            roomDatabaseViewModel.getDailyWaterRecord(currDate)
+            Log.d("TAG", "DataGraph: $currDate - ${dateRecordMapper.value[currDate]}")
+            dateRecordMapper.value[currDate]?.date?.let { it1 ->
+              dateRecordMapper.value[currDate]?.currWaterAmount?.let { it2 ->
+                dateRecordMapper.value[currDate]?.goal?.let { it3 ->
+                  Bar(
+                    date = it1,
+                    waterAmount = it2,
+                    waterGoal = it3,
+                    setSelectedDate = setSelectedDate,
+                    selectedDate = selectedDate
+                  )
+                }
               }
             }
-          }
 
-          currDate = DateString.getPrevDate(currDate)
+            currDate = DateString.getPrevDate(currDate)
+          }
         }
+      }
+    }
+
+    if(listState.firstVisibleItemIndex >= 4){
+      /*TODO("Need to optimize Lazy Row for this to work")*/
+      IconButton(onClick = { /*TODO*/ }) {
+        Icon(
+          painter = painterResource(id = R.drawable.up_arrow),
+          contentDescription = "Scroll to 0"
+        )
       }
     }
   }
