0f482ea461a451ded66a3c0f365e79285c158768
Sameep Sehgal <sameepsam34@gmail.com>
2022-02-13 18:14:10 +0530

    Wrote tests for ReminderReceiverUtilTest.kt to cover all startTime, currTime, and endTime cases, deleted Models

--- app/src/test/java/com/example/myapplication/utils/ReminderReceiverUtilTest.kt
+++ app/src/test/java/com/example/myapplication/utils/ReminderReceiverUtilTest.kt
@@ -1,14 +1,159 @@
 package com.example.myapplication.utils
 
+import com.example.myapplication.data.models.DailyWaterRecord
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.After
+import org.junit.Before
+import java.util.*
 
 class ReminderReceiverUtilTest {
 
-  @org.junit.Before
+  private lateinit var currTime: Calendar
+  private lateinit var startTime: String
+  private lateinit var endTime: String
+  private var remindAfterGoalAchieved: Boolean = false
+  private lateinit var todaysWaterRecord: DailyWaterRecord
+
+  @Before
   fun setUp() {
+    currTime = Calendar.getInstance()
+    startTime = ReminderPeriod.DEFAULT_REMINDER_PERIOD_START
+    endTime = ReminderPeriod.DEFAULT_REMINDER_PERIOD_END
+    remindAfterGoalAchieved = false
+    todaysWaterRecord = DailyWaterRecord(goal = 2500)
   }
 
-  @org.junit.After
+  @After
   fun tearDown() {
+  }
+
+  /*
+  * currTime == "04:30"
+  * startTime == "06:00"
+  * endTime == "22:00"
+   */
+  @Test
+  fun currTimeLessThanStartTimeAndBothStartAndEndTimeOnSameDay_returnsFalse() {
+    currTime.set(Calendar.HOUR_OF_DAY, 4)
+    currTime.set(Calendar.MINUTE, 30)
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertFalse(res)
+  }
+
+  /*
+  * currTime == "10:30"
+  * startTime == "06:00"
+  * endTime == "22:00"
+   */
+  @Test
+  fun currTimeBetweenStartTimeAndEndTimeAndBothStartAndEndTimeOnSameDay_returnsTrue() {
+    currTime.set(Calendar.HOUR_OF_DAY, 10)
+    currTime.set(Calendar.MINUTE, 30)
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertTrue(res)
+  }
+
+  /*
+  * currTime == "23:30"
+  * startTime == "06:00"
+  * endTime == "22:00"
+   */
+  @Test
+  fun currTimeGreaterThanEndTimeAndBothStartAndEndTimeOnSameDay_returnsFalse() {
+    currTime.set(Calendar.HOUR_OF_DAY, 23)
+    currTime.set(Calendar.MINUTE, 30)
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertFalse(res)
+  }
+
+  /*
+  * currTime == "10:30"
+  * startTime == "06:00"
+  * endTime == "02:00" next day
+   */
+  @Test
+  fun currTimeBetweenStartTimeAndEndTimeAndEndTimeOnNextDay_returnsTrue() {
+    currTime.set(Calendar.HOUR_OF_DAY, 10)
+    currTime.set(Calendar.MINUTE, 30)
+    endTime = "02:00"
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertTrue(res)
+  }
+
+  /*
+  * currTime == "04:30"
+  * startTime == "06:00"
+  * endTime == "02:00" next day
+   */
+  @Test
+  fun currTimeLessThanStartTimeAndEndTimeOnNextDay_returnsFalse() {
+    currTime.set(Calendar.HOUR_OF_DAY, 4)
+    currTime.set(Calendar.MINUTE, 30)
+    endTime = "02:00"
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertFalse(res)
+  }
+
+  /*
+  * currTime == "01:00"
+  * startTime == "06:00"
+  * endTime == "02:00" next day
+   */
+  @Test
+  fun currTimeBetweenStartTimeAndEndTimeAndStartTimeOnPreviousDate_returnsTrue() {
+    currTime.set(Calendar.HOUR_OF_DAY, 10)
+    currTime.set(Calendar.MINUTE, 30)
+    endTime = "02:00"
+
+    val res = ReminderReceiverUtil.shallNotify(
+      reminderPeriodStart = startTime,
+      reminderPeriodEnd = endTime,
+      remindAfterGoalAchieved = remindAfterGoalAchieved,
+      todaysWaterRecord = todaysWaterRecord,
+      currTime = currTime
+    )
+
+    assertTrue(res)
   }
 }
