431f302669dfb07ce6d32ff05bf5f3c475529a89
Sameep Sehgal <sameepsam34@gmail.com>
2022-02-25 10:33:07 +0530

    TurnReminderOnReceiver.kt now turn reminder notification on

--- app/src/main/java/com/sameep/watertracker/remindernotification/TurnReminderOnReceiver.kt
+++ app/src/main/java/com/sameep/watertracker/remindernotification/TurnReminderOnReceiver.kt
@@ -3,11 +3,12 @@ package com.sameep.watertracker.remindernotification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import kotlinx.coroutines.flow.first
 import androidx.core.app.NotificationManagerCompat
 import androidx.datastore.preferences.core.edit
 import com.sameep.watertracker.data.preferencedatastore.PreferenceDataStore
 import com.sameep.watertracker.data.preferencedatastore.dataStore
+import com.sameep.watertracker.utils.ReminderReceiverUtil
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -15,10 +16,22 @@ class TurnReminderOnReceiver: BroadcastReceiver() {
   override fun onReceive(context: Context?, intent: Intent?) {
     if (context != null) {
       NotificationManagerCompat.from(context).cancel(NOTIFICATION_ID)
-    }
-    GlobalScope.launch {
-      context?.dataStore?.edit {
-        it[PreferenceDataStore.PreferencesKeys.IS_REMINDER_ON] = true
+      var reminderPeriodStartTime: String? = null
+      var reminderGap: Int? = null
+      GlobalScope.launch {
+        context.dataStore.data.first{
+          reminderPeriodStartTime = it[PreferenceDataStore.PreferencesKeys.REMINDER_PERIOD_START]
+          reminderGap = it[PreferenceDataStore.PreferencesKeys.REMINDER_GAP]
+          true
+        }
+        ReminderReceiverUtil.setBothReminderAndAlarm(
+          context = context,
+          reminderGap = reminderGap!!,
+          reminderPeriodStartTime = reminderPeriodStartTime!!
+        )
+        context.dataStore.edit {
+          it[PreferenceDataStore.PreferencesKeys.IS_REMINDER_ON] = true
+        }
       }
     }
   }
