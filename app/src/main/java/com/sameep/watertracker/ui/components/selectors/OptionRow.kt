1ba658173aec04cb44ccb79df964d39fbac25101
Sameep Sehgal <sameepsam34@gmail.com>
2022-02-28 23:27:36 +0530

    Changed OptionRow.kt to contain Rounded Checkbox

--- app/src/main/java/com/sameep/watertracker/ui/components/selectors/OptionRow.kt
+++ app/src/main/java/com/sameep/watertracker/ui/components/selectors/OptionRow.kt
@@ -1,13 +1,19 @@
 package com.sameep.watertracker.ui.components.selectors
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sameep.watertracker.ui.theme.Typography
 
 @Composable
@@ -18,20 +24,49 @@ fun OptionRow(
   center:Boolean = false
 ) {
   Row(
-    modifier = Modifier.clickable {
-      onClick()
-    }.fillMaxWidth(),
+    modifier = Modifier
+      .clickable {
+        onClick()
+      }
+      .fillMaxWidth()
+      .padding(4.dp, 8.dp),
     verticalAlignment = Alignment.CenterVertically,
     horizontalArrangement = if(center) Arrangement.Center else Arrangement.Start
   ) {
-    Checkbox(
-      checked = selected,
-      onCheckedChange = { onClick() },
-      colors = CheckboxDefaults.colors(checkedColor = MaterialTheme.colors.primary)
-    )
+    Check(selected = selected)
     Text(
       text = text,
-      style = Typography.subtitle1
+      style = Typography.subtitle1,
+      modifier = Modifier.padding(4.dp,0.dp,0.dp,0.dp)
     )
+  }
+}
+
+@Composable
+fun Check(
+  selected: Boolean,
+) {
+  val modifier = Modifier
+    .clip(CircleShape)
+    .size(22.dp)
+    .background(MaterialTheme.colors.onSurface)
+    .padding(1.5.dp)
+  Box(
+    modifier =
+    if(selected)
+      modifier
+        .clip(CircleShape)
+        .background(MaterialTheme.colors.primary)
+    else modifier
+      .clip(CircleShape)
+      .background(MaterialTheme.colors.surface),
+    contentAlignment = Alignment.Center
+  ) {
+    if(selected)
+      Icon(
+        imageVector = Icons.Default.Check,
+        contentDescription = "",
+        tint = Color.White
+      )
   }
 }
