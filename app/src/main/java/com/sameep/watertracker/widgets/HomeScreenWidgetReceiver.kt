4f1eff503c8149db00967c86fa834c0b8599ddc4
Sameep Sehgal <sameep.sehgal@cred.club>
2023-03-18 14:41:04 +0530

    Created Basic Layout for widget

--- app/src/main/java/com/sameep/watertracker/widgets/HomeScreenWidgetReceiver.kt
+++ app/src/main/java/com/sameep/watertracker/widgets/HomeScreenWidgetReceiver.kt
@@ -1,8 +1,13 @@
 package com.sameep.watertracker.widgets
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.sameep.watertracker.MainActivity
+import com.sameep.watertracker.R
 
 class HomeScreenWidgetReceiver : AppWidgetProvider() {
 
@@ -11,6 +16,22 @@ class HomeScreenWidgetReceiver : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
+        appWidgetIds?.forEach { appWidgetId ->
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                Intent(context, MainActivity::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
 
+            val views: RemoteViews = RemoteViews(
+                context?.packageName,
+                R.layout.home_widget_layout
+            ).apply {
+                setOnClickPendingIntent(R.id.home_widget_open_app_button, pendingIntent)
+            }
+
+            appWidgetManager?.updateAppWidget(appWidgetId, views)
+        }
     }
 }
