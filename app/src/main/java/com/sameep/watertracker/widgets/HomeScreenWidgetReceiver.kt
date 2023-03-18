0c23f880a67d0923f1ac2ac598407ee7662a3e53
Sameep Sehgal <sameep.sehgal@cred.club>
2023-03-18 12:12:42 +0530

    Added basic setup for widget

---
+++ app/src/main/java/com/sameep/watertracker/widgets/HomeScreenWidgetReceiver.kt
@@ -0,0 +1,16 @@
+package com.sameep.watertracker.widgets
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class HomeScreenWidgetReceiver : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+
+    }
+}
